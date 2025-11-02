c1ea3bd9d19955f042bde2a1ba7c1c74b998bdbc
Alex Cameron <asc@tetsuo.sh>
2025-11-02 13:43:08 +1100

    feat(forth-dialect): add stack type and core operations

--- include/warpforth/Dialect/Forth/ForthDialect.h
+++ include/warpforth/Dialect/Forth/ForthDialect.h
@@ -7,5 +7,13 @@
 #pragma once
 
 #include "mlir/IR/Dialect.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
 
 #include "warpforth/Dialect/Forth/ForthOpsDialect.h.inc"
+
+#define GET_TYPEDEF_CLASSES
+#include "warpforth/Dialect/Forth/ForthOpsTypes.h.inc"
+
+#define GET_OP_CLASSES
+#include "warpforth/Dialect/Forth/ForthOps.h.inc"
--- lib/Dialect/Forth/ForthDialect.cpp
+++ lib/Dialect/Forth/ForthDialect.cpp
@@ -7,17 +7,28 @@
 #include "warpforth/Dialect/Forth/ForthDialect.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/DialectImplementation.h"
+#include "llvm/ADT/TypeSwitch.h"
 
 using namespace mlir;
 using namespace mlir::forth;
 
 #include "warpforth/Dialect/Forth/ForthOpsDialect.cpp.inc"
 
+#define GET_TYPEDEF_CLASSES
+#include "warpforth/Dialect/Forth/ForthOpsTypes.cpp.inc"
+
+#define GET_OP_CLASSES
+#include "warpforth/Dialect/Forth/ForthOps.cpp.inc"
+
 //===----------------------------------------------------------------------===//
 // Forth dialect.
 //===----------------------------------------------------------------------===//
 
 void ForthDialect::initialize() {
+  addTypes<
+#define GET_TYPEDEF_LIST
+#include "warpforth/Dialect/Forth/ForthOpsTypes.cpp.inc"
+      >();
   addOperations<
 #define GET_OP_LIST
 #include "warpforth/Dialect/Forth/ForthOps.cpp.inc"
